d78c85fd09ef66f9b5ad993d09c4b11b336f8413
Nico Beck <nicobeck97@gmail.com>
2024-12-18 16:51:59 +0100

    corrections

--- chapter2/kernel-modules/gcd/gcd.c
+++ chapter2/kernel-modules/gcd/gcd.c
@@ -16,8 +16,14 @@ int gcd_init(void) {
     return 0;
 }
 
-int gcd_exit(void) {
+void gcd_exit(void) {
     unsigned long x = gcd(3300ul, 24u);
     printk(KERN_INFO "Computation... gcd(3300, 24) = %lu\n", x);
     printk(KERN_INFO "Removing Kernel Module gcd\n");
 }
+
+module_init(gcd_init);
+module_exit(gcd_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GCD Module");
+MODULE_AUTHOR("Nico");
